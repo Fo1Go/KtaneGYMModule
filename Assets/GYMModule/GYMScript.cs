9b3880a8e55cdd69f723c42c0d6fcaa41873e770
Usevalad <vsevolod.prostyakov@krainet.by>
2026-01-04 19:19:13 +0300

    Initn

---
+++ Assets/GYMModule/GYMScript.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GYMScript : MonoBehaviour {
+	public KMBombModule module;
+	public KMBombInfo bomb;
+	public KMAudio sound;
+	
+	private int _moduleID;
+	private static int _moduleIDCounter = 1;
+	private bool _isModuleSolved;
+	void Start () {
+		
+	}
+
+}
